64510822a4111f9e3bd742661e4ad520ddf110a6
Lucas <lukasferreiradesousa890@gmail.com>
2021-12-04 11:13:45 -0300

    versão final

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const pesquisaCidade = async() => {
-    const cidade = document.getElementById('cidade').value
-    const urlSvg = `http://localhost:3000/getSvg/${cidade}`
-    const dadosSvg = await fetch(urlSvg)
-    const svg = await dadosSvg.json()
-    const path = document.querySelector('#SVG')
-    path.setAttribute("d", svg[0].st_assvg)
+    try{
+        const cidade = document.getElementById('cidade').value
+        const urlSvg = `http://localhost:3000/getSvg/${cidade}`
+        const dadosSvg = await fetch(urlSvg)
+        const svg = await dadosSvg.json()
+        const path = document.querySelector('#SVG')
+        path.setAttribute("d", svg[0].st_assvg)
 
-    const urlViewBox = `http://localhost:3000/getViewBox/${cidade}`
-    const dadosViewBox = await fetch(urlViewBox)
-    const viewBox = await dadosViewBox.json()
-    const view = document.querySelector("#vbox")
-    view.setAttribute("viewBox", viewBox[0].getviewbox)
+        const urlViewBox = `http://localhost:3000/getViewBox/${cidade}`
+        const dadosViewBox = await fetch(urlViewBox)
+        const viewBox = await dadosViewBox.json()
+        const view = document.querySelector("#vbox")
+        view.setAttribute("viewBox", viewBox[0].getviewbox)
+    }catch (e) {
+        console.log("Error")
+        console.log(e)
+    }
 }
 
-document.getElementById('cidade').addEventListener('focusout', pesquisaCidade)
